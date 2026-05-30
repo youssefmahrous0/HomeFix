9b955704eaad72d8ad9730a9e56d2d4ef6a4d9e7
Youssef Mahrous <youremail@gmail.com>
2026-05-30 16:28:42 +0300

    added a new line to the footer component

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -11,32 +11,21 @@ export default function Footer() {
       <div className="container mx-auto">
         <div dir="ltr" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 text-center md:text-right">
 
-          {/* تواصل معنا */}
-         <div>
-            <h3 className="font-bold text-lg mb-4">تواصل معنا</h3>
+           {/* logo */}
+          <div className="lg:order-4">
+            <div className="flex items-center gap-2 md:justify-end justify-center mb-3">
+              <span className="text-2xl font-bold">HomeFix</span>
+              <div className="w-10 h-10 bg-green-600 rounded-full flex items-center justify-center text-white">
+                <img src={footerLogo} alt="HomeFix Logo" className="w-6 h-6" />
+              </div>
+            </div>
 
-            <ul className="space-y-3 text-gray-600">
-              <li className="flex items-center md:justify-end justify-center gap-2">
-                +966 50 123 4567
-                <img src={phoneIcon} alt="Phone" className="w-5 h-5" />
-              </li>
-
-              <li className="flex items-center md:justify-end justify-center gap-2">
-                info@homefix.com
-                <img src={emailIcon} alt="Email" className="w-5 h-5" />
-              </li>
-
-              <li className="flex items-center md:justify-end justify-center gap-2">
-                القاهرة، مصر
-                <img src={locationIcon} alt="Location" className="w-5 h-5" />
-              </li>
-            </ul>
+            <p className="text-gray-600 leading-7"> منصة شاملة تربطك بأفضل مقدمي الخدمات المنزلية في منطقتك بكل سهولة وأمان . </p>
           </div>
 
-
- {/*مساعدة ودعم */}
+{/*مساعدة ودعم */}
  
-<div>
+<div className="lg:order-2">
   <h3 className="font-bold text-lg mb-4">المساعدة والدعم</h3>
 
   <ul className="space-y-2 text-gray-500">
@@ -65,8 +54,9 @@ export default function Footer() {
     </li>
   </ul>
 </div>
-          {/* روابط سريعة */}
-          <div>
+
+{/* روابط سريعة */}
+          <div className="lg:order-3">
             <h3 className="font-bold text-lg mb-4">روابط سريعة</h3>
 
             <ul className="space-y-2 text-gray-500">
@@ -101,21 +91,28 @@ export default function Footer() {
   </li>
 </ul>
           </div>
+          {/* تواصل معنا */}
+         <div className="lg:order-1">
+            <h3 className="font-bold text-lg mb-4">تواصل معنا</h3>
 
-          {/* logo */}
-          <div>
-            <div className="flex items-center gap-2 md:justify-end justify-center mb-3">
-              <span className="text-2xl font-bold">HomeFix</span>
-              <div className="w-10 h-10 bg-green-600 rounded-full flex items-center justify-center text-white">
-                <img src={footerLogo} alt="HomeFix Logo" className="w-6 h-6" />
-              </div>
-            </div>
+            <ul className="space-y-3 text-gray-600">
+              <li className="flex items-center md:justify-end justify-center gap-2">
+                +966 50 123 4567
+                <img src={phoneIcon} alt="Phone" className="w-5 h-5" />
+              </li>
 
-            <p className="text-gray-600 leading-7"> منصة شاملة تربطك بأفضل مقدمي الخدمات المنزلية في منطقتك بكل سهولة وأمان . </p>
+              <li className="flex items-center md:justify-end justify-center gap-2">
+                info@homefix.com
+                <img src={emailIcon} alt="Email" className="w-5 h-5" />
+              </li>
+
+              <li className="flex items-center md:justify-end justify-center gap-2">
+                القاهرة، مصر
+                <img src={locationIcon} alt="Location" className="w-5 h-5" />
+              </li>
+            </ul>
           </div>
-
         </div>
-
 
         {/* bottom */}
         <div
