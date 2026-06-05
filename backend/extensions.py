c6096b2665473e430f331a9c2ae105d6dc08ccf9
Youssef Mahrous <youremail@gmail.com>
2026-06-05 15:27:02 +0300

    Fix SocketIO

--- backend/extensions.py
+++ backend/extensions.py
@@ -3,7 +3,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_socketio import SocketIO
 
-socketio = SocketIO( cors_allowed_origins="*", async_mode="eventlet")
+socketio = SocketIO(cors_allowed_origins="*", async_mode="threading")
 
 # Database
 db = SQLAlchemy()
